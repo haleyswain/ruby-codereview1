30d828714c9666c8a25cc8d0ff28c7e1ea8695db
Haley  Swain <haleyswain@Haleys-MacBook-Pro.local>
2016-11-18 14:57:04 -0800

    repair method so that words containing inputted words are not counted in the result

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,6 +1,7 @@
 class String
-  define_method(:word_count) do |word|
-    search = self.downcase.scan(word)
-    search.length
+  define_method(:word_count) do |phrase|
+    downcased_input = self.downcase()
+    input = downcased_input.split(" ")
+    input = input.count(phrase)
   end
 end
--- spec/wordcount_spec.rb
+++ spec/wordcount_spec.rb
@@ -6,9 +6,9 @@ describe('String#word_count') do
   it('will recognize whether a word is repeated in 2 user inputs') do
     expect('cat'.word_count('cat')).to(eq(1))
   end
-  it('will return the number of times an inputted word appears consistent with the number of times it is repeated in a multiple word phrase') do
-    expect('the cat went to the cat store to pick up cat food'.word_count('cat')).to(eq(3))
+  it('will return the number of times an inputted word appears consistent with the number of times it is repeated in a multiple word phrase without counting words that contain the inputted word') do
+    expect('the cat went to the cathedral to pick up cat food'.word_count('cat')).to(eq(2))
   end
-  
+
 
 end
