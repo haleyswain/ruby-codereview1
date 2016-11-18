8f9969aa1c54b58e96501adb6fa27641d81f785b
Haley  Swain <haleyswain@Haleys-MacBook-Pro.local>
2016-11-18 08:24:20 -0800

    update views files

--- spec/wordcount_spec.rb
+++ spec/wordcount_spec.rb
@@ -3,7 +3,12 @@ require('pry')
 require('word_count')
 
 describe('String#word_count') do
-  it('will return the repeated word consistent with the number of times it is repeated') do
+  it('will recognize whether a word is repeated in 2 user inputs') do
+    expect('cat'.word_count('cat')).to(eq(1))
+  end
+  it('will return the number of times an inputted word appears consistent with the number of times it is repeated in a multiple word phrase') do
     expect('the cat went to the cat store to pick up cat food'.word_count('cat')).to(eq(3))
   end
+  
+
 end
